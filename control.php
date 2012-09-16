001fa8dc7265bd2f6233cb3463a3353bc3cbd7bc
xLink <xLink@cybershade.org>
2012-09-16 14:13:12 +0100

    started porting in the cache system, made changes for multiple DBO's on the queryBuilder

---
+++ control.php
@@ -0,0 +1,12 @@
+<?php
+/*======================================================================*\
+||                 Cybershade CMS - Your CMS, Your Way                  ||
+\*======================================================================*/
+define('INDEX_CHECK', true);
+define('cmsDEBUG', true);
+include_once('core/core.php');
+
+echo dump($objCache);
+echo dump($objSQL);
+
+?>
