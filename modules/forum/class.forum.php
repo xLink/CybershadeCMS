d120c7cc204966873e182aed22a446af08489a86
Dan Aldridge <xLink@cybershade.org>
2012-09-22 18:04:15 +0100

    fixed __call in forum

--- modules/forum/class.forum.php
+++ modules/forum/class.forum.php
@@ -6,9 +6,8 @@ defined('INDEX_CHECK') or die('Error: Cannot access directly.');
 
 class forum extends Module{
 
-    public function __call(){
-        $a = func_get_args();
-        echo dump($a, 'Arguments Called in forum module');
+    public function __call($name, $arguments){
+        echo dump($arguments, 'Called forum::'.$name);
     }
 
 
