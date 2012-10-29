ad65c7fafc9845a1276ff01f1cb63c1da0e7a67b
Dan Aldridge <xLink@cybershade.org>
2012-10-29 21:16:59 +0000

    added config backup

---
+++ assets/config.bak.php
@@ -0,0 +1,32 @@
+<?php
+/*======================================================================*\
+||              Cybershade CMS - Your CMS, Your Way                     ||
+\*======================================================================*/
+if(!defined('INDEX_CHECK')){die('Error: Cannot access directly.');}
+
+//some db settings and the like etc
+    $config['db']['driver']        = 'mysqli';
+    #$config['db']['driver']        = 'mysql';
+    $config['db']['host']          = 'localhost';
+    $config['db']['username']      = 'root';
+    $config['db']['password']      = '';
+    $config['db']['database']      = 'cscms';
+    $config['db']['port']          = '3306';
+    $config['db']['prefix']        = 'cscms_';
+//the cookie prefix
+    $config['db']['ckefix']        = 'CMS_';
+
+//some settings for the cron
+    $config['cron']['hourly_time'] = (3600); //1 Hour
+    $config['cron']['daily_time']  = (3600*24); //1 Day
+    $config['cron']['weekly_time'] = (3600*24*7); //1 Week
+
+//some default settings, incase the cms dies before getting
+//the chance to populate the config array.
+    $config['cms']['name']         = 'Cybershade CMS';
+    $config['cms']['version']      = 'N/A';
+    $config['site']['title']       = 'Cybershade CMS';
+    $config['site']['theme']       = 'cybershade';
+    $config['site']['language']    = 'en';
+
+?>
