4829a853d90d12bbe264b83c782d994773cd50ea
Richard Clifford <darkmantis@cybershade.org>
2012-11-06 14:41:33 +0000

    Added the new Permissions class and updated the User class slightly

---
+++ core/classes/class.permissions.php
@@ -0,0 +1,136 @@
+<?php
+/*======================================================================*\
+||                 Cybershade CMS - Your CMS, Your Way                  ||
+\*======================================================================*/
+defined('INDEX_CHECK') or die('Error: Cannot access directly.');
+
+
+class Permissions extends coreObj {
+
+    static  $IS_ADMIN   = false,
+            $IS_MOD     = false,
+            $IS_USER    = false,
+            $IS_SPECIAL = false; // For various, custom permissions
+
+    /**
+     * Defines global CMS permissions
+     *
+     * @version 1.0
+     * @since   1.0.0
+     * @author  Dan Aldridge
+     */
+    public function initPerms(){
+        self::$IS_USER      = $this->checkPermissions($this->get('id'), USER);
+        self::$IS_ADMIN     = $this->checkPermissions($this->get('id'), ADMIN);
+        self::$IS_MOD       = $this->checkPermissions($this->get('id'), MOD);
+    }
+
+    /**
+     * Returns permission state for given user and group
+     *
+     * @version 1.0
+     * @since   1.0.0
+     * @author  Dan Aldridge
+     *
+     * @param   int     $uid        UserID
+     * @param   int     $group      GUEST, USER, MOD, or ADMIN
+     *
+     * @return  bool    True/False on successful check, -1 on unknown group
+     */
+    public function checkUserAuth( $type, $key, $u_access, $is_admin ){
+        $auth_user = 0;
+
+        if(count($u_access)){
+            for($j = 0; $j < count($u_access); $j++){
+                $result = 0;
+                switch($type){
+                    case AUTH_ACL:   $result = $u_access[$j][$key]; break;
+                    case AUTH_MOD:   $result = $result || $u_access[$j]['auth_mod']; break;
+                    case AUTH_ADMIN: $result = $result || $is_admin; break;
+                }
+                $auth_user = $auth_user || $result;
+            }
+        }else{
+            $auth_user = $is_admin;
+        }
+        return $auth_user;
+    }
+
+    /**
+     * Returns permission state for given user and group
+     *
+     * @version 1.0
+     * @since   1.0.0
+     * @author  Dan Aldridge
+     *
+     * @param   int     $uid        UserID
+     * @param   int     $group      GUEST, USER, MOD, or ADMIN
+     *
+     * @return  bool    True/False on successful check, -1 on unknown group
+     */
+    public function checkPermissions( $uid, $group=0 ) {
+        $group = (int)$group;
+
+        //make sure we have a group to check against
+        if(is_empty($group) || $group == 0 || $group == GUEST){
+            return true;
+        }
+
+        //check to see whether we have a user id to check against..
+        if(is_empty($uid)){
+            return false;
+        }
+
+        //grab the user level if possible
+        $userlevel = GUEST;
+
+        // Get the user Object
+        $objUser = coreObj::getUser();
+
+        if(self::$IS_ONLINE){
+            $userlevel = $objUser->getUserInfo($uid, 'userlevel');
+        }
+
+        //see which group we are checking for
+        switch($group){
+            case GUEST:
+                if(!self::$IS_ONLINE){
+                    return true;
+                }
+            break;
+
+            case USER:
+                if(self::$IS_ONLINE){
+                    return true;
+                }
+            break;
+
+            case MOD:
+                if($userlevel == MOD){
+                    return true;
+                }
+            break;
+
+            case ADMIN:
+                if($userlevel == ADMIN){
+                    if(LOCALHOST || doArgs('adminAuth', false, $_SESSION['acp'])){
+                        return true;
+                    }
+                }
+            break;
+
+            //no idea what they tried to check for, so we'll return something unexpected too
+            default: return -1; break;
+        }
+
+        //if we are an admin then give them mod powers regardless
+        if(($group == MOD || $group == USER) && $userlevel == ADMIN){
+            return true;
+        }
+
+        //apparently the checks didnt return true, so we'll go for false
+        return false;
+    }
+}
+
+?>
--- core/classes/class.user.php
+++ core/classes/class.user.php
@@ -10,11 +10,6 @@ class User extends coreObj {
     //some static vars
     static  $IS_ONLINE  = false;
 
-    static  $IS_ADMIN   = false,
-            $IS_MOD     = false,
-            $IS_USER    = false,
-            $IS_SPECIAL = false; // For various, custom permissions
-
     public function __construct(){
 
         $guest['user'] = array(
@@ -60,18 +55,7 @@ class User extends coreObj {
         return self::$IS_ONLINE = $value;
     }
 
-    /**
-     * Defines global CMS permissions
-     *
-     * @version 1.0
-     * @since   1.0.0
-     * @author  Dan Aldridge
-     */
-    public function initPerms(){
-        self::$IS_USER      = $this->checkPermissions($this->get('id'), USER);
-        self::$IS_ADMIN     = $this->checkPermissions($this->get('id'), ADMIN);
-        self::$IS_MOD       = $this->checkPermissions($this->get('id'), MOD);
-    }
+
 
 /**
   //
@@ -469,8 +453,38 @@ class User extends coreObj {
         return true;
     }
 
-    public function setPassword( $uid, $password ){
 
+    /**
+     * Sets a user password to a new value
+     *
+     * @version 1.0.0
+     * @since   1.0.0
+     * @author  Richard Clifford
+     *
+     * @param int    $uid
+     * @param string $password
+     *
+     * @return bool
+     */
+    public function setPassword( $uid, $password ){
+        if( is_empty( $password ) ){
+            return false;
+        }
+        $objSQL = coreObj::getDBO();
+
+        $query = $objSQL->queryBuilder()
+                        ->update('#__users')
+                        ->set( 'password', $this->mkPassword( $password ) )
+                        ->where('id', $uid)
+                        ->build();
+
+        $result = $objSQL->query( $query );
+
+        if( $result ){
+            return true;
+        }
+
+        return false;
     }
 
     public function toggle( $var, $state=null ){
@@ -488,19 +502,51 @@ class User extends coreObj {
     }
 
     public function canLogin( $username, $password ){
-
+        // Check if username exists
+        // Check if password is correct for user
+        // Check if session is not banned
     }
 
+
+
+    /**
+     * Makes a secure password
+     *
+     * @version 1.0.0
+     * @since   1.0.0
+     * @author  Richard Clifford
+     *
+     * @param string $password
+     * @param string $salt
+     *
+     * @return string
+     */
     public function mkPassword( $password, $salt='' ) {
         $objPass = new phpass( 8, true );
 
         $hashed = $objPass->hashPassword( $salt . $password );
 
-        if( $hashed < 20  ) {
-
+        if( strlen($hashed) >= 20 ) {
+            return $hashed;
         }
+
+        // Not sure if this is what is wanted
+        return '';
     }
 
+
+    /**
+     * Makes a secure password
+     *
+     * @version 1.0.0
+     * @since   1.0.0
+     * @author  Dan Aldridge
+     *
+     * @param string $username
+     * @param string $password
+     *
+     * @return bool
+     */
     public function verifyUserCredentials( $username, $password ) {
         $objSQL = coreObj::getDBO();
 
@@ -522,117 +568,9 @@ class User extends coreObj {
 
         } else {
             return false;
-
         }
     }
-/**
-  //
-  //-- Auth Functions
-  //
-**/
 
-    /**
-     * Returns permission state for given user and group
-     *
-     * @version 1.0
-     * @since   1.0.0
-     * @author  Dan Aldridge
-     *
-     * @param   int     $uid        UserID
-     * @param   int     $group      GUEST, USER, MOD, or ADMIN
-     *
-     * @return  bool    True/False on successful check, -1 on unknown group
-     */
-    public function checkUserAuth( $type, $key, $u_access, $is_admin ){
-        $auth_user = 0;
-
-        if(count($u_access)){
-            for($j = 0; $j < count($u_access); $j++){
-                $result = 0;
-                switch($type){
-                    case AUTH_ACL:   $result = $u_access[$j][$key]; break;
-                    case AUTH_MOD:   $result = $result || $u_access[$j]['auth_mod']; break;
-                    case AUTH_ADMIN: $result = $result || $is_admin; break;
-                }
-                $auth_user = $auth_user || $result;
-            }
-        }else{
-            $auth_user = $is_admin;
-        }
-        return $auth_user;
-    }
-
-    /**
-     * Returns permission state for given user and group
-     *
-     * @version 1.0
-     * @since   1.0.0
-     * @author  Dan Aldridge
-     *
-     * @param   int     $uid        UserID
-     * @param   int     $group      GUEST, USER, MOD, or ADMIN
-     *
-     * @return  bool    True/False on successful check, -1 on unknown group
-     */
-    public function checkPermissions( $uid, $group=0 ) {
-        $group = (int)$group;
-
-        //make sure we have a group to check against
-        if(is_empty($group) || $group == 0 || $group == GUEST){
-            return true;
-        }
-
-        //check to see whether we have a user id to check against..
-        if(is_empty($uid)){
-            return false;
-        }
-
-        //grab the user level if possible
-        $userlevel = GUEST;
-        if(self::$IS_ONLINE){
-            $userlevel = $this->getUserInfo($uid, 'userlevel');
-        }
-
-        //see which group we are checking for
-        switch($group){
-            case GUEST:
-                if(!self::$IS_ONLINE){
-                    return true;
-                }
-            break;
-
-            case USER:
-                if(self::$IS_ONLINE){
-                    return true;
-                }
-            break;
-
-            case MOD:
-                if($userlevel == MOD){
-                    return true;
-                }
-            break;
-
-            case ADMIN:
-                if($userlevel == ADMIN){
-                    if(LOCALHOST || doArgs('adminAuth', false, $_SESSION['acp'])){
-                        return true;
-                    }
-                }
-            break;
-
-            //no idea what they tried to check for, so we'll return something unexpected too
-            default: return -1; break;
-        }
-
-        //if we are an admin then give them mod powers regardless
-        if(($group == MOD || $group == USER) && $userlevel == ADMIN){
-            return true;
-        }
-
-        //apparently the checks didnt return true, so we'll go for false
-        return false;
-    }
 }
 
 ?>
