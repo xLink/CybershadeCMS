edba82157c6cf154999b66879357a5ccc242e343
Richard Clifford <darkmantis@cybershade.org>
2012-11-15 22:04:32 +0000

    Uploader class working & tested. user_class.php there for PoC

--- user_test.php
+++ user_test.php
@@ -6,11 +6,36 @@ define('INDEX_CHECK', true);
 define('cmsDEBUG', true);
 
 include_once('core/core.php');
+$objRoute = coreObj::getRoute();
+$objPage  = coreObj::getPage();
+$objTPL   = coreObj::getTPL();
+
+$objPage->setTheme();
+
+$objPage->setTitle('Test');
+
+$objRoute->processURL( $_SERVER['QUERY_STRING'] );
+
 
 $objUpload = coreObj::getUpload();
-$objForm = coreObj::getForm();
+$objForm   = coreObj::getForm();
 
-echo $objForm->start('test', array( 'upload' => true ));
+$c = $objUpload->makePublic(1);
+echo dump( $c );
+
+echo $objForm->start('test', array( 'method' => 'post', 'upload' => true, 'action' => $_SERVER['PHP_SELF'] ));
+echo $objForm->inputBox('file', 'file', 'file');
+echo $objForm->inputBox('submit', 'submit', 'Submit!');
 echo $objForm->finish();
+
+if( isset( $_POST['submit'] ) ){
+	$var = $objUpload->doUpload( array('txt') );
+	echo dump( $var );
+}
+
+$objPage->buildPage();
+$objPage->showHeader();
+$objPage->showFooter();
+
 
 ?>
