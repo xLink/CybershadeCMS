16e2e4a92279a88355ba83cf93d3e81463a24952
Dan Aldridge <xLink@cybershade.org>
2012-11-19 17:44:20 +0000

    adding it to the default theme too :/

---
+++ themes/default/details.php
@@ -0,0 +1,39 @@
+<?php
+/*======================================================================*\
+||                 Cybershade CMS - Your CMS, Your Way                  ||
+\*======================================================================*/
+defined('INDEX_CHECK') or die('Error: Cannot access directly.');
+
+class Details_cybershade implements baseDetails{
+
+    public function details(){
+        return array(
+            'version'              => '1.0',
+            'since'                => '1.0',
+            'min_version_required' => '1.0',
+
+            'name'                 => 'default',
+            'mode'                 => 'user',
+            'author'               => 'xLink',
+            'homepage_url'         => 'http://cybershade.org',
+            'repo_url'             => 'http://github.com/cybershade/cscms/',
+        );
+    }
+
+    public function getBlocks(){
+        return array(
+
+        );
+    }
+
+    public function install(){
+
+
+    }
+
+    public function uninstall(){
+
+    }
+
+}
+?>
