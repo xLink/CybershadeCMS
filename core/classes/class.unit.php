9e74979a7d1b332d8443ba970f8e9d7f97b46376
Richard Clifford <darkmantis@cybershade.org>
2012-12-20 14:35:38 +0000

    Added the unit testing file and updated the unit test class

--- core/classes/class.unit.php
+++ core/classes/class.unit.php
@@ -6,30 +6,28 @@ defined('INDEX_CHECK') or die('Error: Cannot access directly.');
 
 class Unit extends coreObj{
 
-    public function __construct(){
+    protected $_reportData = array();
 
+    public function __construct(){
+        $this->setVar('strictMode', false);
     }
 
 
     /**
-     * Runs the unit tests and generates a report
+     * Runs the unit tests
      *
      * @version 1.0
      * @since   1.0.0
      * @author  Richard Clifford
      *
-     * @param   mixed   $test
+     * @param   mixed   $testItem
      * @param   string  $expectedResult
      * @param   string  $testName
      * @param   string  $notes
      *
-     * @return  bool    Generates a report using the generateReport() function which returns a bool
+     * @return  obj     $this
      */
-    public function run( $test, $expectedResult, $testName = '', $notes = '' ){
-        if(is_empty( $test ) || is_empty( $epxectedResult )){
-            return false;
-        }
-
+    public function test( $testItem, $expectedResult, $testName = '', $notes = '' ){
         // Allowed results
         $allowedResults = array(
             'is_object',
@@ -42,59 +40,149 @@ class Unit extends coreObj{
             'is_float',
             'is_double',
             'is_array',
-            'is_null'
+            'is_null',
         );
 
         // are we using strict mode?
         $strict = $this->getVar('strictMode');
+        $result = false;
 
-        /**
-        //
-        //-- Todo: loop through the setTestItems array and append $test to that array and perform the tests on them
-        //
-        */
         // Start the checking!
         if( in_array( $expectedResult, $allowedResults, true ) ){
+
             $expectedResult = str_replace('is_float', 'is_double', $expectedResult);
             $result         = ($expectedResult($testItem) ? true : false);
+
+            // Get the result type
             $resultType     = str_replace(array('true', 'false'), 'bool', str_replace('is_', '', $expectedResult) );
         } else {
+            // Explicitly check the types
             if($strict){
                 $result = ( ( $testItem === $expectedResult ) ? true : false );
             } else {
                 $result = ( ( $testItem == $expectedResult ) ? true : false );
             }
+
+            // Get the result type
+            $resultType = gettype( $expectedResult );
         }
 
+
         $report = array(
-            'test_name' => $test,
-            'test_datatype' => gettype($test),
+            'test_name' => ( is_null($testItem) ? 'NULL' : $testItem ),
+            'test_datatype' => gettype($testItem),
             'result_datatype' => $resultType,
-            'result' => ($result === true ? 'Passed.', 'Failed.'),
+            'result' => ($result === true ? 'Passed.' : 'Failed.'),
             'file' => __FILE__,
             'line' => __LINE__,
             'notes' => $notes,
+            'raw_result' => $result,
         );
 
-        $generateReport = $this->_generateReport($report);
-
-        return $generateReport;
-    }
-
-    public function setTestItms( $testItems = array() ){
-        if( !is_empty($testItems) ){
-            $this->setVar('testItems', $testItems);
-        }
+        $this->_reportData[] = $report;
 
         return $this;
     }
 
+
+    /**
+     * Sets the use mode to STRICT to explicitly check the data types
+     *
+     * @version 1.0
+     * @since   1.0.0
+     * @author  Richard Clifford
+     *
+     * @return  obj     $this
+     */
     public function useStrict(){
         $this->setVar('strictMode', true);
         return $this;
     }
 
-    protected function _generateReport( $reportData ){
-        return true;
+    /**
+     * Runs the tests and generates a report
+     *
+     * @version 1.0
+     * @since   1.0.0
+     * @author  Richard Clifford
+     *
+     * @return  string  HTML Table string
+     */
+    public function run(){
+        return $this->_generateReport( $this->_reportData );
+    }
+
+
+    /**
+     * Generates the HTML Table Report
+     *
+     * @version 1.0
+     * @since   1.0.0
+     * @author  Richard Clifford
+     *
+     * @access  protected
+     *
+     * @return  string  HTML Table string
+     */
+    protected function _generateReport( $reportData = array() ){
+
+        $_reportData = $this->getVar('_reportData');
+
+        if( !is_empty($_reportData) ){
+            $reportData = $_reportData;
+        }
+
+        if( is_empty( $reportData ) ){
+            return false;
+        }
+
+        /**
+         //
+         // -- Put into a new template rather than from this class
+         //
+         */
+        $output = '<table class="table table-condensed">
+                        <thead>
+                            <tr>
+                                <th>Test Name</th>
+                                <th>Expected Result</th>
+                                <th>Actual Result Type</th>
+                                <th>Result</th>
+                                <th>File</th>
+                                <th>Line Number</th>
+                                <th>Notes</th>
+                            </tr>
+                        </thead>
+                        <tbody>';
+
+        foreach( $reportData as $key => $value ){
+            $output .= sprintf('
+                        <tr class="%s">
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                        </tr>',
+                    !$value['raw_result'] ? 'error' : 'success',
+                    $value['test_name'],
+                    $value['test_datatype'],
+                    $value['result_datatype'],
+                    $value['result'],
+                    $value['file'],
+                    $value['line'],
+                    $value['notes']
+                    );
+        }
+
+        $output .=  '</tbody>
+                    </table>';
+        echo dump( $output );
+
+        return $output;
     }
 }
+
+?>
---
+++ unit_testing.php
@@ -0,0 +1,39 @@
+<?php
+/*======================================================================*\
+||                 Cybershade CMS - Your CMS, Your Way                  ||
+\*======================================================================*/
+define('INDEX_CHECK', true);
+define('cmsDEBUG', true);
+set_time_limit(0);
+require_once 'core/core.php';
+
+$objRoute = coreObj::getRoute();
+$objPage  = coreObj::getPage();
+$objTPL   = coreObj::getTPL();
+
+$objPage->setTheme();
+
+$objPage->setTitle('Test');
+
+$objModule = $objRoute->processURL($_SERVER['QUERY_STRING']);
+
+$objPage->buildPage();
+$objPage->showHeader();
+
+if ( $objModule !== false ) {
+    $objModule->output();
+}
+
+$objUnit = coreObj::getUnit();
+
+$a = '1';
+$b = array('test');
+$c = NULL;
+
+echo $objUnit->useStrict()
+    ->test( $a, 'is_string')
+    ->test( $b, 'is_bool', 'test', 'Array me pl0x')
+    ->test( $c, 'is_bool', 'test', 'NULLIFY')
+    ->run();
+
+?>
