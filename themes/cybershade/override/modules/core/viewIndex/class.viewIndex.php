adbd1922fda54c4f5e129bebb0dc910b8918f99c
xLink <xlink@cybershade.org>
2013-02-21 12:42:33 +0000

    added the override example for the cs theme

---
+++ themes/cybershade/override/modules/core/viewIndex/class.viewIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+class Override_Modules_core_viewIndex extends Modules_core{
+
+    // public function viewIndex(){
+    //     echo dump($a, 'OVERRIDE INITITATED!!');
+    //     $this->setView('viewIndex/default.tpl');
+
+    // }
+
+
+}
+?>
