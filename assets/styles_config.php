4892dc65727bbe1f76ae42f9528ee97ec66b78da
xLink <xlink@cybershade.org>
2013-04-11 21:57:44 +0100

    updating default styles, form controls show correctly now

--- assets/styles_config.php
+++ assets/styles_config.php
@@ -1,7 +1,7 @@
 <?php
 $styles = array();
-$styles['framework'] = array('default.css', 'debug.css', 'famfam.css', 'chosen.css');
-$styles['bootstrap'] = array('bootstrap.min.css', 'bootstrap-responsive.min.css');
+$styles['framework'] = array('debug.css', 'famfam.css', 'chosen.css');
+$styles['bootstrap'] = array('bootstrap.min.css', 'bootstrap-responsive.min.css', 'default.css');
 $styles['extras'] = array('font-awesome.css');
 
 $styles['panels'] = array();
