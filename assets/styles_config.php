ccbcce689ead9047fdf8c2db1e8b658672e17e13
user <user@users-MacBook-Pro.local>
2013-04-11 10:59:51 +0100

    Removed default.css from core, as it was screwing with the working css

--- assets/styles_config.php
+++ assets/styles_config.php
@@ -1,6 +1,6 @@
 <?php
 $styles = array();
-$styles['framework'] = array('default.css', 'debug.css', 'famfam.css', 'chosen.css');
+$styles['framework'] = array(/*'default.css',*/ 'debug.css', 'famfam.css', 'chosen.css');
 $styles['bootstrap'] = array('bootstrap.min.css', 'bootstrap-responsive.min.css');
 $styles['extras'] = array('font-awesome.css');
 
