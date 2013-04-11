9ea8c937149b0653d8064fc4f61848aee9174477
xLink <xlink@cybershade.org>
2013-04-12 00:14:15 +0100

    plugins updated, able to pass by reference in the args array

--- core/classes/class.plugins.php
+++ core/classes/class.plugins.php
@@ -47,34 +47,38 @@ class Core_Classes_Plugins extends Core_Classes_coreObj{
         if($this->dontExec == true){ return false; }
         $objSQL = Core_Classes_coreObj::getDBO();
 
-        //make sure we didnt get an empty var...
+        // make sure we didn't get an empty var...
         if(!is_array($plugins) || is_empty($plugins)){
-            //if we did try and get a fresh copy from the db
+            // if we did try and get a fresh copy from the db
             $objCache = Core_Classes_coreObj::getCache();
 
             $plugins = $objCache->load('plugins');
 
             if(!is_array($plugins) || is_empty($plugins)){
                 $this->dontExec = true;
-                return false; //no luck this time so just return quietly
+                return false; // no luck this time so just return quietly
             }
         }
 
-        //loop though each plugin
-        foreach($plugins as $hook){
+        // loop though each plugin
+        foreach( $plugins as $hook ){
             $hookStr = $hook['path'];
 
-            //make sure its actually a file and is readable
-            if(!is_file($hookStr) && !is_readable($hookStr)){ continue; }
+            // make sure its actually a file and is readable
+            if( !is_file($hookStr) && !is_readable($hookStr) ){ 
+                continue; 
+            }
 
-            //also make sure its enabled..
-            if(!$hook['enabled']){ continue; }
+            // also make sure its enabled..
+            if( $hook['enabled'] === false ){ 
+                continue; 
+            }
 
-            //and then include it :D
+            // and then include it :D
             include_once( str_replace('./', cmsROOT.'', $hookStr) );
         }
 
-        //everything worked as expected so just return true;
+        // everything worked as expected so just return true;
         return true;
     }
 
@@ -86,57 +90,100 @@ class Core_Classes_Plugins extends Core_Classes_coreObj{
      * @author      Dan Aldridge
      *
      * @param       string     $hook
-     * @param       string     $args
+     * @param       mixed      $args
      * @param       string     $option
      * @param       int        $priority
      *
      * @return      string
      */
     public function hook($hook, &$args='', $option='run', $priority=MED){
-        //decide what we need to do here
-        switch($option){
+        // decide what we need to do here
+        switch( $option ){
             case 'run':
                 $hooks = $this->hooks;
-                //make sure we have something to run with
-                if(!is_array($hooks) || is_empty($hooks)){ return; }
+                $result = array();
+                $func = '';
 
-                if( !in_array( $hook, $this->availableHooks ) ) {
+                // make sure we have something to run with
+                if( !is_array($hooks) || is_empty($hooks) ){ return; }
+
+                if( !in_array($hook, $this->availableHooks) ) {
                     $this->availableHooks[] = $hook;
                 }
 
-                //loop though each 'priority'
-                foreach(array('1', '2', '3') as $prio){
-                    if(!isset($hooks[$hook][$prio]) || !is_array($hooks[$hook][$prio]) || is_empty($hooks[$hook][$prio])){ continue; }
+                // loop though each 'priority'
+                foreach( array('1', '2', '3') as $prio ){
+                    if( !isset($hooks[$hook][$prio]) || !is_array($hooks[$hook][$prio]) || is_empty($hooks[$hook][$prio]) ){ 
+                        continue; 
+                    }
 
                     // and then each hook
-                    while(current($hooks[$hook][$prio])){
-                        //get func name
-                        $function = key($hooks[$hook][$prio]);
-                        $cb = '';
+                    while( current($hooks[$hook][$prio]) ){
+                        // get func name
+                        $func = key($hooks[$hook][$prio]);
+                        $return = null;
 
-                        //make sure we can call it still
-                        if(is_callable($function)){ $cb = $function($args); }
+                        // see if we want to call to a class or not
+                        $test = json_decode($func, true);
+                        if( $test !== null ){
+                            $function = $test;
+                        } else{
+                            $function = $func;
+                        }
 
-                        //check to see if we got a response from the func, this should be true
-                        if(is_empty($cb)){ $cb = false; }
+                        // make sure we can call it still
+                        if( is_callable( $function ) ){ 
 
-                        //assign it to the array and continue
-                        $result[$hook][$prio][$function] = $cb;
+                            // if class, then reflect method, else just reflect function
+                            if( is_array($function) && count($function) > 1 ){
+                                $ref = new ReflectionMethod( $function[0], $function[1] );
+                            }else{
+                                $ref = new ReflectionFunction( $function );
+                            }
+
+                            // grab a list of arguments
+                            $vars = array();
+                            $params = $ref->getParameters();
+                            foreach( $params as $k => $name ) {
+                                // and then check if we have to throw the var at them as a reference
+                                if( $name->isPassedByReference() ){
+                                    $vars[$k] = &$args[$k];
+                                }else{
+                                    $vars[$k] = $args[$k];
+                                }
+                            }
+
+                            // invoke the args and continue
+                            if( is_array($function) && count($function) > 1 ){
+                                $return = $ref->invokeArgs( new $function[0], $vars );
+                            }else{
+                                $return = $ref->invokeArgs( $vars );
+                            }
+   
+                        }
+
+                        // check to see if we got a response from the func, this should be true
+                        if( is_empty($return) ){ 
+                            $return = false; 
+                        }
+
+                        // assign it to the array and continue
+                        $result[$hook][$prio][$func] = $return;
                         next($hooks[$hook][$prio]);
                      }
                 }
-                if( isset($function) && isset( $result[$hook][$priority][$function] ) ) {
-                    return $result[$hook][$priority][$function];
+                if( isset($func) && isset( $result[$hook][$priority][$func] ) ) {
+                    return $result[$hook][$priority][$func];
                 }
             break;
 
             case 'add':
-                //register the hook with the system
+                // register the hook with the system
                 $this->hooks[$hook][$priority][$args] = 'fail';
             break;
 
             case 'rm':
-                //remove the hook from the system
+                // remove the hook from the system
                 unset($this->hooks[$hook][$priority][$args]);
             break;
         }
@@ -154,6 +201,10 @@ class Core_Classes_Plugins extends Core_Classes_coreObj{
      * @param       int     $priority
      */
     public function addHook($hook, $callback, $priority=MED){
+        if( is_array($callback) && count($callback) > 1 ){
+            $callback = json_encode($callback);
+        }
+
         $this->hook($hook, $callback, 'add', $priority);
     }
 
