9592dba1d30b38b228daf10eb9f49c4b0688bbe6
xLink <xlink@cybershade.org>
2013-04-11 21:59:48 +0100

    moved plugins/cms/ to plugins/core/

---
+++ plugins/core/recache.php
@@ -0,0 +1,15 @@
+<?php
+
+function recache(){ 
+    if(isset($_GET['_recache'])){
+        echo dump($_GET, 'RECACHE BOOM!');
+        $objCache = Core_Classes_coreObj::getCache();
+
+        $objCache->remove('stores');
+        $objCache->remove('media');
+        $objCache->remove('template');
+    }
+}
+
+$this->addHook('CMS_PRE_SETUP_COMPLETE', 'recache');
+?>
