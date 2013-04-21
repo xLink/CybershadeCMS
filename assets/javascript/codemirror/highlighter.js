858edc8cb2ef3e03eb760b03c33e58f1556313bf
xLink <xlink@cybershade.org>
2013-04-22 00:36:08 +0100

    reworked the highlighter file for the new syntax

--- assets/javascript/codemirror/highlighter.js
+++ assets/javascript/codemirror/highlighter.js
@@ -1,35 +1,5 @@
 window.addEvent('domready', function() {
 
-//     $code = $$('[data-codemirror="true"]');
-//     if( $code.length ){
-
-//         $code.each(function(ele){
-//             var editor = CodeMirror(
-//                 function(node){
-//                     ele.parentNode.replaceChild(node, ele);
-//                 }, {
-//                     value:              ele.textContent,
-//                     lineNumbers:        true,
-//                     mode:               ele.get('data-lang') || 'text/plain',
-//                     readOnly:           'nocursor',
-//                     theme:              'monokai',
-//                     //styleActiveLine:    true,
-//                     lineWrapping:       true,
-//                     viewportMargin:     Infinity
-//                 }
-//             );
-// console.log(ele.getPrevious('ul'));
-//             // var clip = new ZeroClipboard( ele.getLast('ul.nav-tabs').getChildren('li.copy-code a')[0], {
-//             //   moviePath: cscms.root('libaries/zeroclipboard/ZeroClipboard.swf')
-//             // } );
-
-//             // clip.setText(ele.textContent);
-//             // clip.on('onMouseDown', function(){});
-//             // clip.on('wrongflash', function ( client, args ) {});
-//         });
-
-//     }
-
     $codeBlocks = $$('div.code');
 
     if( $codeBlocks.length ){
