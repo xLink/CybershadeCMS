a2ec5befe543d76aa9469c56ffc624efdd5a2a71
xLink <xlink@cybershade.org>
2013-04-27 15:59:49 +0100

    readded the lowercase version

---
+++ languages/en-gb/main.php
@@ -0,0 +1,101 @@
+<?php
+/*======================================================================*\
+||              Cybershade CMS - Your CMS, Your Way                     ||
+\*======================================================================*/
+if(!defined('INDEX_CHECK')){ die('Error: Cannot access directly.'); }
+if(!isset($LANG_LOAD)){ die('Error: Cannot access directly.'); }
+
+/**
+  //
+  //-- Form Vars
+  //
+**/
+$_lang['L_USERNAME'] = 'Username';
+$_lang['L_PASSWORD'] = 'Password';
+$_lang['L_REMME']    = 'Remember Me';
+/*$_lang[''] = '';
+$_lang[''] = '';*/
+
+
+/**
+  //
+  //-- Login & Registration
+  //
+**/
+$_lang['L_LAST_VISIT']          = 'Last Visit: %s';
+
+/**
+  //
+  //-- Time Variables
+  //
+**/
+$_lang['DATETIME']['Sunday']    = 'Sunday';
+$_lang['DATETIME']['Monday']    = 'Monday';
+$_lang['DATETIME']['Tuesday']   = 'Tuesday';
+$_lang['DATETIME']['Wednesday'] = 'Wednesday';
+$_lang['DATETIME']['Thursday']  = 'Thursday';
+$_lang['DATETIME']['Friday']    = 'Friday';
+$_lang['DATETIME']['Saturday']  = 'Saturday';
+$_lang['DATETIME']['Sun']       = 'Sun';
+$_lang['DATETIME']['Mon']       = 'Mon';
+$_lang['DATETIME']['Tue']       = 'Tue';
+$_lang['DATETIME']['Wed']       = 'Wed';
+$_lang['DATETIME']['Thu']       = 'Thu';
+$_lang['DATETIME']['Fri']       = 'Fri';
+$_lang['DATETIME']['Sat']       = 'Sat';
+$_lang['DATETIME']['January']   = 'January';
+$_lang['DATETIME']['February']  = 'February';
+$_lang['DATETIME']['March']     = 'March';
+$_lang['DATETIME']['April']     = 'April';
+$_lang['DATETIME']['May']       = 'May';
+$_lang['DATETIME']['June']      = 'June';
+$_lang['DATETIME']['July']      = 'July';
+$_lang['DATETIME']['August']    = 'August';
+$_lang['DATETIME']['September'] = 'September';
+$_lang['DATETIME']['October']   = 'October';
+$_lang['DATETIME']['November']  = 'November';
+$_lang['DATETIME']['December']  = 'December';
+$_lang['DATETIME']['Jan']       = 'Jan';
+$_lang['DATETIME']['Feb']       = 'Feb';
+$_lang['DATETIME']['Mar']       = 'Mar';
+$_lang['DATETIME']['Apr']       = 'Apr';
+$_lang['DATETIME']['May']       = 'May';
+$_lang['DATETIME']['Jun']       = 'Jun';
+$_lang['DATETIME']['Jul']       = 'Jul';
+$_lang['DATETIME']['Aug']       = 'Aug';
+$_lang['DATETIME']['Sep']       = 'Sep';
+$_lang['DATETIME']['Oct']       = 'Oct';
+$_lang['DATETIME']['Nov']       = 'Nov';
+$_lang['DATETIME']['Dec']       = 'Dec';
+$_lang['DATETIME']['am']        = 'am';
+$_lang['DATETIME']['pm']        = 'pm';
+
+// Time Class Language Vars
+$_lang['TIMEAGO_SUFFIXAGO']     = ' ago';
+$_lang['TIMEAGO_SUFFIXFROMNOW'] = 'from now';
+$_lang['TIMEAGO_SECONDS']       = 'less than a minute';
+$_lang['TIMEAGO_MINUTE']        = 'about a minute';
+$_lang['TIMEAGO_MINUTES']       = '%d minutes';
+$_lang['TIMEAGO_HOUR']          = 'about an hour';
+$_lang['TIMEAGO_HOURS']         = 'about %d hours';
+$_lang['TIMEAGO_DAY']           = 'a day';
+$_lang['TIMEAGO_DAYS']          = '%d days';
+$_lang['TIMEAGO_WEEK']          = 'a week';
+$_lang['TIMEAGO_WEEKS']         = '%d weeks';
+$_lang['TIMEAGO_MONTH']         = 'about a month';
+$_lang['TIMEAGO_MONTHS']        = '%d months';
+$_lang['TIMEAGO_YEAR']          = 'about a year';
+$_lang['TIMEAGO_YEARS']         = '%d years';
+
+// Registration Vars
+$_lang['L_PASSWORD_CONFIRM']      = 'Confirm Password';
+$_lang['L_EMAIL_ADDRESS']         = 'Email Address';
+$_lang['L_EMAIL_ADDRESS_CONFIRM'] = 'Confirm Email Address';
+$_lang['L_REFERER']               = 'Referer';
+$_lang['L_RECEIVE_EMAILS_ADMINS'] = 'Receive emails from Admins';
+$_lang['L_RECEIVE_EMAILS_USERS']  = 'Receive emails from Users';
+$_lang['L_DISCLAIMER']            = 'Disclaimer';
+
+
+// Control Panels
+include_once('panels.php');
